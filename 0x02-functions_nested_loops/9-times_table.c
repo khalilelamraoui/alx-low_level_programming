c245d2f5842a86f2db1dff251be17b2875387459
El Amraoui Khalil <khalilelam16@gmail.com>
2023-09-12 08:16:01 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,22 +10,24 @@ void times_table(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (j = 1; j < 10; j++)
 		{
-			result = i * j;
-			if (result >= 10)
+			result = (i * j);
+			if ((result / 10) > 0)
 			{
-				_putchar('0' + (result / 10));
+				_putchar((result / 10) + '0');
 			}
 			else
 			{
 				_putchar(' ');
 			}
-			_putchar('0' + result % 10);
+			_putchar((result % 10) + '0');
 			if (j < 9)
 			{
 				_putchar(',');
-				_putchar(' ');
 				_putchar(' ');
 			}
 		}
