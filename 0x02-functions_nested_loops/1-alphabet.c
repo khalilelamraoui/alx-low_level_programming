ab82b1269bc6f962022b096e2ec356f7565a9262
El Amraoui Khalil <khalilelam16@gmail.com>
2023-09-12 05:13:03 -0700

    printing alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include"main.h"
 /**
  * print_alphabet - Entry point
- * description: function for printing alphabets 
+ * description: function for printing alphabets
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
