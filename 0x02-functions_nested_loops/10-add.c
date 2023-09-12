5bf2e3d4f2b09023e2b5de2a1d158caffad543c0
El Amraoui Khalil <khalilelam16@gmail.com>
2023-09-12 08:19:43 -0700

    addition of a and b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include"main.h"
+/**
+ * add - Entry point
+ * description: function that checks if char is lower
+ * @a: first int
+ * @b: second int
+ * Return: a + b (Success)
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
