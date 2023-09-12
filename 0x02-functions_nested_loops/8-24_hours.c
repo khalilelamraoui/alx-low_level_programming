2d4cff7166a02d7a8beebddf1e3857d48d94693e
El Amraoui Khalil <khalilelam16@gmail.com>
2023-09-12 07:16:57 -0700

    hours:minutes

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include"main.h"
+/**
+ * jack_bauer - Entry point
+ * description: function that prints every minute
+ */
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 23; i++)
+	{
+		for (j = 0; j <= 59; j++)
+		{
+			_putchar('0' + i / 10);
+			_putchar('0' + i % 10);
+			_putchar(':');
+			_putchar('0' + j / 10);
+			_putchar('0' + j % 10);
+			_putchar('\n');
+		}
+	}
+}
