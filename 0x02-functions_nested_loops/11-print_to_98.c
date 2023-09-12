ce73f9d51b0d60a03fb845cf854ed7c1fb3c7eed
El Amraoui Khalil <khalilelam16@gmail.com>
2023-09-12 08:47:05 -0700

    print_to_98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,38 +1,39 @@
 #include"main.h"
 /**
- * times_table - Entry point
- * description: function that prints times table
- * Return: Always 0 (Success)
+ * print_to_98 - Entry point
+ * description: function that prints numbers to 98
+ * @n: number to count from
+ * Return: array of nums (Success)
  */
 void print_to_98(int n)
 {
-        int i;
+	int i;
 
-        if (n < 98)
-        {
-                for (i = n; i <= 98; i++)
-                {
-                    	printf("%d", i);
-                        if(i < 98)
-                        {
-                                printf(", ");
-                        }
-                }
-        }
-        if (n == 98)
-        {
-                printf("%d", n);
-        }
-        if (n > 98)
-        {
-                for (i = n; i >= 98; i--)
-                {
-                        printf("%d", i);
-                        if(i > 98)
-                        {
-                                printf(", ");
-                        }
-                }
-        }
-        printf("\n");
+	if (n < 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i < 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	if (n == 98)
+	{
+		printf("%d", n);
+	}
+	if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i > 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	printf("\n");
 }
