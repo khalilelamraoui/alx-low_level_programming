c34c50e0253dc093116d66db0b3abb9f1d0b9687
El Amraoui Khalil <khalilelam16@gmail.com>
2023-09-12 17:30:36 -0700

    fibonacci 2

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Finds and prints the sum of even-valued terms
+ * in the Fibonacci sequence below 4,000,000.
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	long long int first = 1, second = 2, next;
+	long long int sum = 0;
+
+	while (second <= 4000000)
+	{
+		if (second % 2 == 0)
+		{
+			sum += second;
+		}
+
+		next = first + second;
+		first = second;
+		second = next;
+	}
+
+	printf("%lld\n", sum);
+
+    return (0);
+}
