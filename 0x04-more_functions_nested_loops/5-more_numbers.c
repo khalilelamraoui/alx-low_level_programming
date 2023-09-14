843ab856d077cc1dd6340a54990e1fe711709847
El Amraoui Khalil <khalilelam16@gmail.com>
2023-09-14 08:35:32 -0700

    printing numbers from 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,15 +14,11 @@ void more_numbers(void)
 	{
 		for (i = 0; i <= 14; i++)
 		{
-			if (i < 10)
-			{
-				_putchar(i + '0');
-			}
-			else
+			if (i >= 10)
 			{
 				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
 			}
+			_putchar((i % 10) + '0');
 		}
 		_putchar('\n');
 	}
