2bb806f7adec216b98176ef863ff0e45e4101f8a
El Amraoui Khalil <khalilelam16@gmail.com>
2023-09-14 10:30:20 -0700

    print number function using putchar

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,15 +7,17 @@
  */
 void print_number(int n)
 {
-	if (n < 0)
+	unsigned int number = n;
+
+	if (number < 0)
 	{
 		_putchar('-');
-		n = -n;
+		number = -number;
 	}
 
-	if ((n / 10) != 0)
+	if ((number / 10) != 0)
 	{
-		print_number(n / 10);
+		print_number(number / 10);
 	}
-	_putchar((n % 10) + '0');
+	_putchar((number % 10) + '0');
 }
