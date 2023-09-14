cbb67ba6054af8e3c20dd68c2b5111c00b47e14a
El Amraoui Khalil <khalilelam16@gmail.com>
2023-09-14 06:39:18 -0700

    mul function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include"main.h"
+/**
+ * mul - Entry point
+ * description: function that multiplies a by b
+ * @a: first int
+ * @b: second int
+ * Return: a + b (Success)
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
