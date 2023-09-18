d9fd95a815b87fbedd918f024fee1695a6e05ba6
El Amraoui Khalil <khalilelam16@gmail.com>
2023-09-18 02:12:14 -0700

    reset to 98 function

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include"main.h"
+/**
+ * reset_to_98 - writes the character c to stdout
+ * @n: The character to reset
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
