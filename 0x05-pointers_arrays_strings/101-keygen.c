bc345dba9ab92154a54e4ab5f3487772495c57c4
El Amraoui Khalil <khalilelam16@gmail.com>
2023-09-18 08:21:51 -0700

    keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include"main.h"
+#include <stdlib.h>
+#include <time.h>
+
+char RandomCharacter()
+{
+	const char charset[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	int i = rand() % (sizeof(charset) - 1);
+	return charset[i];
+}
+
+int main() {
+	int password_length = 15;
+	srand(time(NULL));
+
+	for (int i = 0; i < password_length; i++)
+	{
+		char randomChar = RandomCharacter();
+		putchar(randomChar);
+	}
+	putchar('\n');
+
+	return 0;
+}
