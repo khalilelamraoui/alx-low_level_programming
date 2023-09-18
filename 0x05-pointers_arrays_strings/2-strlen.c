1848927386d55ae7a483cd8fc0a826614b1df036
El Amraoui Khalil <khalilelam16@gmail.com>
2023-09-18 03:01:46 -0700

    print length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * strlen - Prints length of string
+ * @s: The string for length calculation
+ * description: function that prints length of string
+ * Return: 0 success
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+	
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return i;
+}
