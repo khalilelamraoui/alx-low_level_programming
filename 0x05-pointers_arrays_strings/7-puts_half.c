62b8b1ad8baca810cdf77d847d1687bac5f2f5f3
El Amraoui Khalil <khalilelam16@gmail.com>
2023-09-18 04:38:15 -0700

    printing last half of string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include"main.h"
+/**
+ * _puts - Prints string
+ * @str: The string for length calculation
+ * description: function that prints half-string
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int i;
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i >= len / 2)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
