45fd425c4ee876083ba7c5936c012182a1fef26f
El Amraoui Khalil <khalilelam16@gmail.com>
2023-09-20 07:05:34 -0700

    capitalize words

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,45 @@
+#include"main.h"
+/**
+ * cap_string - entry point
+ * @str: string to capitalize
+ * description: function that capitalizes string
+ * Return: result
+ */
+char *cap_string(char *str)
+{
+	int word = 0, i, is_separator;
+	char separators[] = " \t\n,;.!?\"(){}";
+	char current_char;
+	int capitalize = 1;
+
+	while (str[word] != '\0')
+	{
+		current_char = str[word];
+
+		if (capitalize && current_char >= 'a' && current_char <= 'z')
+		{
+			str[word] -= 32;
+			capitalize = 0;
+		}
+		else if (!capitalize && current_char == '.')
+		{
+			capitalize = 1;
+        	}
+		is_separator = 0;
+		for (i = 0; separators[i] != '\0'; i++)
+		{
+			if (current_char == separators[i])
+			{
+				is_separator = 1;
+				break;
+			}
+		}
+		if (is_separator)
+		{
+			capitalize = 1;
+		}
+
+		word++;
+	}
+	return (str);
+}
