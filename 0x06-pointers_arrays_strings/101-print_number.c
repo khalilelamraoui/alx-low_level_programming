c34ef7596a41fa86cdc15ad2329859c7e3bcd2e4
El Amraoui Khalil <khalilelam16@gmail.com>
2023-09-21 07:36:03 -0700

    printing numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,11 +6,14 @@
  */
 void print_number(int n)
 {
-	int division, modulo;
-
-	division = n / 10;
-	modulo = n % 10;
-	_putchar('0' + division);
-	_putchar('0' + modulo);
-	_putchar('\n');
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+	}
+	_putchar('0' + n % 10);
 }
