12de6c63c8d5505e4a61c85a614d3e62f8247597
El Amraoui Khalil <khalilelam16@gmail.com>
2023-09-21 08:54:35 -0700

    infinite addition

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -12,20 +12,22 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int carry = 0, num1, num2, sum;
 	int i = 0, j = 0, k = 0;
+	char temp;
 
 	while (n1[i] != '\0' || n2[j] != '\0' || carry)
 	{
 		num1 = (n1[i]) ? n1[i] - '0' : 0;
 		num2 = (n2[j]) ? n2[j] - '0' : 0;
 		sum = num1 + num2 + carry;
-        
 		if (k >= size_r - 1)
 			return (0);
 		r[k++] = (sum % 10) + '0';
 		carry = sum / 10;
 
-		if (n1[i]) i++;
-		if (n2[j]) j++;
+		if (n1[i])
+			i++;
+		if (n2[j])
+			j++;
 	}
 
 	r[k] = '\0';
@@ -34,7 +36,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		return (0);
 	for (i = 0, j = k - 1; i < j; i++, j--)
 	{
-		char temp = r[i];
+		temp = r[i];
 		r[i] = r[j];
 		r[j] = temp;
 	}
