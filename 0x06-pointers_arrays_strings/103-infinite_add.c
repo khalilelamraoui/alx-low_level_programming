1d0ea391e0136a15e0d1d9b9ea696d9fe1704e25
El Amraoui Khalil <khalilelam16@gmail.com>
2023-09-21 09:05:47 -0700

    infinite addition

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -8,35 +8,41 @@
  *
  * Return: A pointer to the result string, or 0 if the result can't fit
  */
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
-{
-	int carry = 0, num1, num2, sum;
-	int i = 0, j = 0, k = 0;
-	char temp;
+char *infinite_add(char *n1, char *n2, char *r, int size_r) {
+	int len1 = 0, len2 = 0;
+	int i, j, k = 0, num1, num2, sum;
+	int carry = 0;
 
-	while (n1[i] != '\0' || n2[j] != '\0' || carry)
+	while (n1[len1] != '\0')
 	{
-		num1 = (n1[i]) ? n1[i] - '0' : 0;
-		num2 = (n2[j]) ? n2[j] - '0' : 0;
+		len1++;
+	}
+	while (n2[len2] != '\0')
+	{
+		len2++;
+	}
+	if (len1 > size_r || len2 > size_r)
+		return (0);
+	i = len1 - 1;
+	j = len2 - 1;
+	while (i >= 0 || j >= 0 || carry)
+	{
+		num1 = (i >= 0) ? n1[i] - '0' : 0;
+		num2 = (j >= 0) ? n2[j] - '0' : 0;
 		sum = num1 + num2 + carry;
-		if (k >= size_r - 1)
-			return (0);
-		r[k++] = (sum % 10) + '0';
 		carry = sum / 10;
-
-		if (n1[i])
-			i++;
-		if (n2[j])
-			j++;
+		r[k++] = (sum % 10) + '0';
+		i--;
+		j--;
 	}
 	r[k] = '\0';
 	if (k >= size_r)
 		return (0);
 	for (i = 0, j = k - 1; i < j; i++, j--)
 	{
-		temp = r[i];
+		char temp = r[i];
 		r[i] = r[j];
 		r[j] = temp;
 	}
-	return (r);
+    return (r);
 }
