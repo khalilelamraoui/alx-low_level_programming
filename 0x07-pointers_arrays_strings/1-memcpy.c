8d77efe6d62f909513909aef6356408692095ea4
El Amraoui Khalil <khalilelam16@gmail.com>
2023-09-25 06:07:44 -0700

    memcpy function

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/*
+ * _memcpy: function that returns copy of string
+ * @dest: destination
+ * @src: source
+ * @n: size of elements
+ * return: result
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	char *result = dest;
+	
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (result);
+}
