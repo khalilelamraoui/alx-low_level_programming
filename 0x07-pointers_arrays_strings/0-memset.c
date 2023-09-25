8cca42952d1d70d48bc0ea0bcfa9d144426d1d55
El Amraoui Khalil <khalilelam16@gmail.com>
2023-09-25 05:49:43 -0700

    memset function

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+/**
+ * _memset - function that replaces s elements with b chars
+ * @s: string to work on
+ * @b: char wanted to output
+ * @n: number of first elements wanted
+ * Return: result : s.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+	
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
