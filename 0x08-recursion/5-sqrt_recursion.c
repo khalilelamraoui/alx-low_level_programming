25181a321e0fa608b059eb05300d5de450f3d3da
El Amraoui Khalil <khalilelam16@gmail.com>
2023-09-28 06:38:10 -0700

    sqrt recursive

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,17 +9,17 @@
  */
 int _sqrt_recursive(int n, int low, int high)
 {
-    int mid;
+	int mid;
 
-    if (low > high)
-        return (-1);
-    mid = (low + high) / 2;
-    if (mid * mid == n)
-        return (mid);
-    else if (mid * mid < n)
-        return (_sqrt_recursive(n, mid + 1, high));
-    else
-        return (_sqrt_recursive(n, low, mid - 1));
+	if (low > high)
+                return (-1);
+	mid = (low + high) / 2;
+	if (mid * mid == n)
+		return (mid);
+	else if (mid * mid < n)
+		return (_sqrt_recursive(n, mid + 1, high));
+	else
+		return (_sqrt_recursive(n, low, mid - 1));
 }
 /**
  * _sqrt_recursion - returns the natural square root of a number
@@ -29,10 +29,9 @@ int _sqrt_recursive(int n, int low, int high)
  */
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-        return (-1);
-    if (n == 0 || n == 1)
-        return (n);
-
-    return (_sqrt_recursive(n, 1, n));
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (n);
+	return (_sqrt_recursive(n, 1, n));
 }
