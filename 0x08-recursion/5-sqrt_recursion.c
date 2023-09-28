43ae3ffb1992e502c01bf0e51183fef41420370e
El Amraoui Khalil <khalilelam16@gmail.com>
2023-09-28 06:42:42 -0700

    sqrt recursive

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,24 +2,17 @@
 /**
  * _sqrt_recursive - Recursive helper function to calculate square root.
  * @n: The number for which the square root is to be calculated.
- * @low: The lower bound for the guess.
- * @high: The upper bound for the guess.
+ * @guess: The current guess for the square root.
  *
  * Return: The natural square root of n, or -1 if n does not have one.
  */
-int _sqrt_recursive(int n, int low, int high)
+int _sqrt_recursive(int n, int guess)
 {
-	int mid;
-
-	if (low > high)
-		return (-1);
-	mid = (low + high) / 2;
-	if (mid * mid == n)
-		return (mid);
-	else if (mid * mid < n)
-		return (_sqrt_recursive(n, mid + 1, high));
-	else
-		return (_sqrt_recursive(n, low, mid - 1));
+	if (guess * guess == n)
+		return (guess);
+	if (guess * guess < n)
+		return (_sqrt_recursive(n, guess + 1));
+	return (-1);
 }
 /**
  * _sqrt_recursion - returns the natural square root of a number
@@ -33,5 +26,5 @@ int _sqrt_recursion(int n)
 		return (-1);
 	if (n == 0 || n == 1)
 		return (n);
-	return (_sqrt_recursive(n, 1, n));
+	return (_sqrt_recursive(n, 1));
 }
