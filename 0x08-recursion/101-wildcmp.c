f22b38c9c28daf4fa08579ab8ad49ce7530d238d
El Amraoui Khalil <khalilelam16@gmail.com>
2023-09-28 07:44:45 -0700

    wild comparison recursive

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -19,6 +19,6 @@ int wildcmp(char *s1, char *s2)
 	if (*s1 == '\0' || *s2 == '\0')
 		return ((*s1 == *s2));
 	if (*s1 == *s2 || *s2 == '?')
-		return wildcmp(s1 + 1, s2 + 1);
+		return (wildcmp(s1 + 1, s2 + 1));
 	return (0);
 }
