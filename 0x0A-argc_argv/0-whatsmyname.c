f9a9ffc65d1f5f278f9e769a8de69c2be0d2870c
khalilelamraoui <khalilelam16@gmail.com>
2023-10-02 13:48:53 -0700

    name of program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/*
+ * main - entry point.
+ * @argc: number of arguments.
+ * @argv: string.
+ * Return: name of program.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc >= 1)
+		printf("%s\n", argv[0]);
+	return (0);
+}
