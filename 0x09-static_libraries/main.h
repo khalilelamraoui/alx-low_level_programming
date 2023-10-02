42e1987d62ed9dfab31705256f8cdd11096cacc9
khalilelamraoui <khalilelam16@gmail.com>
2023-10-02 11:15:38 -0700

    updated last

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
 #include <stddef.h>
 #include<stdio.h>
 #include<unistd.h>
@@ -21,3 +23,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+#endif
