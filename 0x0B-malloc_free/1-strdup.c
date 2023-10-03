88ca1a47cd8e29945b3a893dc723cbb79cc33fa4
khalilelamraoui <khalilelam16@gmail.com>
2023-10-03 08:42:37 -0700

    copy of str allocated in new memory

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,7 +12,8 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
-	for (size = 0; str[size] != '\0'; size++);
+	for (size = 0; str[size] != '\0'; size++)
+		;
 	ptr = (char *)malloc(sizeof(char) * (size + 1));
 	if (ptr == NULL)
 		return (NULL);
