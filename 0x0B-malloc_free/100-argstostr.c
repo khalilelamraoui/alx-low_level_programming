6c37c43fa1abac13ebf585ad3eb8b30a1b8f5802
khalilelamraoui <khalilelam16@gmail.com>
2023-10-03 10:29:08 -0700

    args to str

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -6,43 +6,38 @@
  *
  * Return: A pointer to the concatenated string, or NULL if it fails
  */
-char *argstostr(int ac, char **av) {
-	int i, length = 0, id = 0;
-	char *arg, *result;
+char *argstostr(int ac, char **av)
+{
+        int i, length = 0, id = 0;
+        char *arg, *result;
 
-	if (ac == 0 || av == NULL)
-		return (NULL);
-	for (i = 0; i < ac; i++) {
-		arg = av[i];
-		while (*arg != '\0')
-		{
-			length++;
-			arg++;
-		}
-		length++;
-	}
-	length += strlen(av[0]) + 1;
-	result = malloc((length + 1) * sizeof(char));
-	if (result == NULL)
-	{
-		free(result);
-		return (NULL);
-	}
-	strcpy(result, av[0]);
-	id = strlen(av[0]);
-
-	for (i = 1; i < ac; i++)
-	{
-		result[id] = '\n';
-		id++;
-		arg = av[i];
-		while (*arg != '\0')
-		{
-			result[id] = *arg;
-			id++;
-			arg++;
-		}
-	}
-	result[id] = '\0';
-	return (result);
+        if (ac == 0 || av == NULL)
+                return (NULL);
+        for (i = 0; i < ac; i++)
+        {
+                arg = av[i];
+                while (*arg != '\0')
+                {
+                        length++;
+                        arg++;
+                }
+                length++;
+        }
+        result = (char *)malloc(sizeof(char) * length);
+        if (result == NULL)
+                return (NULL);
+        for (i = 0; i < ac; i++)
+        {
+                arg = av[i];
+                while (*arg != '\0')
+                {
+                        result[id] = *arg;
+                        id++;
+                        arg++;
+                }
+                result[id] = '\n';
+                id++;
+        }
+        result[id - 1] = '\0';
+        return (result);
 }
