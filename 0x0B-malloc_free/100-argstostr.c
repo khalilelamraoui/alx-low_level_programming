d098163562a1f14c00e1c7a98942d18477cc002e
khalilelamraoui <khalilelam16@gmail.com>
2023-10-03 10:08:53 -0700

    args to str

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * argstostr - Concatenates all arguments with newline separators
+ * @ac: The argument count
+ * @av: An array of argument strings
+ *
+ * Return: A pointer to the concatenated string, or NULL if it fails
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, length = 0, id = 0;
+	char *arg, *result;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		arg = av[i];
+		while (*arg != '\0')
+		{
+			length++;
+			arg++;
+		}
+		length++;
+	}
+	result = (char *)malloc(sizeof(char) * length);
+	if (result == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		arg = av[i];
+		while (*arg != '\0')
+		{
+			result[id] = *arg;
+			id++;
+			arg++;
+		}
+		result[id] = '\n';
+		id++;
+	}
+	result[id - 1] = '\0';
+	result[i] = '\n';
+	return (result);
+}
