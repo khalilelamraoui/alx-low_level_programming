6f5ee5e69bf08625755c6bee60180b6ce330b29e
khalilelamraoui <khalilelam16@gmail.com>
2023-10-07 03:58:53 -0700

    concatenate 2 strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,4 +1,4 @@
-#include"main.h"
+#include "main.h"
 /**
  * string_nconcat - Concatenate two strings with a limit on the second string.
  * @s1: The first string.
@@ -6,13 +6,12 @@
  * @n: The maximum number of bytes from s2 to concatenate.
  *
  * Return: Pointer to the concatenated string, or NULL on failure.
- */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+ */char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int s1_len = strlen(s1);
 	unsigned int s2_len = strlen(s2);
-	unsigned int total_len = s1_len + n;
-	char *result = (char *)malloc(total_len + 1);
+	unsigned int total_len;
+	char *result;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -20,10 +19,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 	if (n >= s2_len)
 		n = s2_len;
+
+	total_len = s1_len + n;
+
+	result = (char *)malloc(total_len + 1);
+
 	if (result == NULL)
-		return (NULL);
+		return NULL;
+
 	strcpy(result, s1);
 	strncat(result, s2, n);
 
-	return (result);
+	return result;
 }
