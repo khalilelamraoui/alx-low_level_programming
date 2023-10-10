98444840c93a8a09426dbb231616d394881cb08d
khalilelamraoui <khalilelam16@gmail.com>
2023-10-10 07:43:43 -0700

    create new dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -22,8 +22,6 @@ struct dog
 	char *owner;
 };
 
-typedef struct dog dog_t;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
