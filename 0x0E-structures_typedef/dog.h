9451bea9e1bc6d0834fb3f81ee760321114f69c0
khalilelamraoui <khalilelam16@gmail.com>
2023-10-10 07:42:26 -0700

    create new dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,10 @@
 #define DOG_H
 
 /**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+/**
  * struct dog - Structure representing a dog
  * @name: Pointer to a character array (dog's name)
  * @age: Age of the dog (float)
@@ -23,5 +27,6 @@ typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
