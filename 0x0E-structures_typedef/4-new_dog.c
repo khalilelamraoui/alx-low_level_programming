cee43bdfd1f9bb90f7b95f83d4559d13fb0b4938
khalilelamraoui <khalilelam16@gmail.com>
2023-10-10 07:53:21 -0700

    fixed new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -8,36 +8,28 @@
  * @age: Age of the dog (float)
  * @owner: Pointer to a character array (owner's name)
  *
- * Return: Pointer to the new dog (struct dog)
- * or NULL if memory allocation fails
+ * Return: Pointer to the new dog (struct dog) or NULL if memory allocation fails
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
-	int name_len = 0, owner_len = 0;
 
-	new_dog = malloc(sizeof(*new_dog));
-	if (new_dog == NULL && name == NULL && owner == NULL)
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+	new_dog->name = strdup(name);
+	if (new_dog->name == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
-	while (name[name_len])
-		name_len++;
-	while (owner[owner_len])
-		owner_len++;
-	new_dog->name = malloc(name_len + 1);
-	new_dog->owner = malloc(owner_len + 1);
-	if (!(new_dog->name) || !(new_dog->owner))
+	new_dog->owner = strdup(owner);
+	if (new_dog->owner == NULL)
 	{
 		free(new_dog->name);
-		free(new_dog->owner);
 		free(new_dog);
 		return (NULL);
 	}
-	new_dog->name = name;
-	new_dog->owner = owner;
 	new_dog->age = age;
-
 	return (new_dog);
 }
