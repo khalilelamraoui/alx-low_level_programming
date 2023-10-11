10f1f308fd4608d278e833bee409567ee3f8bac8
khalilelamraoui <khalilelam16@gmail.com>
2023-10-11 07:36:19 -0700

    fixed op_functions.c betty style

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,17 +1,44 @@
 #include "3-calc.h"
-
+/**
+ * op_add - Returns the sum of two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The sum of the two integers.
+ */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+/**
+ * op_sub - Returns the difference between two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The difference between the two integers.
+ */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+/**
+ * op_mul - Returns the product of two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The product of the two integers.
+ */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+/**
+ * op_div - Returns the result of integer division.
+ * @a: The dividend.
+ * @b: The divisor.
+ *
+ * Return: The result of the division.
+ */
 int op_div(int a, int b)
 {
 	if (b == 0)
@@ -21,6 +48,13 @@ int op_div(int a, int b)
 	}
 	return (a / b);
 }
+/**
+ * op_mod - Returns the remainder of integer division.
+ * @a: The dividend.
+ * @b: The divisor.
+ *
+ * Return: The remainder of the division.
+ */
 int op_mod(int a, int b)
 {
 	if (b == 0)
