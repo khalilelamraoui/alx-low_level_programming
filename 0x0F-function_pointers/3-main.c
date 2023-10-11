b036b3793acf636f90d78f475147bc5dcadf79e1
khalilelamraoui <khalilelam16@gmail.com>
2023-10-11 07:30:23 -0700

    multiple functions

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+	int result;
+	int (*calc_func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	calc_func = get_op_func(argv[2]);
+
+	if (calc_func == NULL)
+	{
+		printf("Error\n");
+		return (99);
+	}
+
+	result = calc_func(num1, num2);
+	printf("%d\n", result);
+
+	return (0);
+}
