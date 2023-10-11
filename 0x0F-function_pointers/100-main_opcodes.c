84d2b839f19e9cc947f1dd3e780dec9920e35b35
khalilelamraoui <khalilelam16@gmail.com>
2023-10-11 11:09:49 -0700

    print opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	int i, num_bytes;
+	unsigned char *main_ptr = (unsigned char *)main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	num_bytes = atoi(argv[1]);
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+	for (i = 0; i < num_bytes; i++)
+	{
+		printf("%02x", main_ptr[i]);
+		if (i < num_bytes - 1)
+			printf(" ");
+	}
+
+	printf("\n");
+	return (0);
+}
