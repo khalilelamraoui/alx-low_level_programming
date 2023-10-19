5777ce5b293a59b341f9ee309bc0917a6d17788a
khalilelamraoui <khalilelam16@gmail.com>
2023-10-19 15:51:34 -0700

    fixed betty

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
-void pre_main_message(void) {
-    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+/**
+ * pre_main_message - Prints a pre-main message.
+ */
+void pre_main_message(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
 }
