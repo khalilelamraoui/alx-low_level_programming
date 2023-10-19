987f0956eb7915a0946c9de171f44fcf8a504b39
khalilelamraoui <khalilelam16@gmail.com>
2023-10-19 14:14:29 -0700

    added print list function

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,3 +1,6 @@
+#ifndef LIST_H
+#define LIST_H
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -8,9 +11,11 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
+
+#endif
