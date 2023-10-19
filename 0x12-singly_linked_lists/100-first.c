df26d66b519da72d0a8794b25d0422c133d3dd7b
khalilelamraoui <khalilelam16@gmail.com>
2023-10-19 15:31:15 -0700

    print string before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void pre_main_message(void) {
+    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
---
+++ 0x12-singly_linked_lists/100-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void pre_main_message(void);
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	pre_main_message();
+	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+	return (0);
+}
