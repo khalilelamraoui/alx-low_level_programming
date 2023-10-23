cf1f7c11ddb2f814aa94f17b7c2958d5eee35b02
khalilelamraoui <khalilelam16@gmail.com>
2023-10-23 11:40:18 -0700

    sum_listint func fix

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 /**
- * sum_listint - Returns the sum of all the data (n) of a listint_t linked list.
+ * sum_listint - Returns the sum of all the data (n) of
+ * a listint_t linked list.
  * @head: A pointer to the head of the list
  * Return: The sum of all data, or 0 if the list is empty
  */
