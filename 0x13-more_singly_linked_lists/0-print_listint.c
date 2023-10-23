9b255025c0a42560248a1b872c6b8bc5b3dc3f3c
khalilelamraoui <khalilelam16@gmail.com>
2023-10-23 11:49:50 -0700

    fixed 0-print_listint func

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,11 +9,6 @@ size_t print_listint(const listint_t *h)
 {
 	int count = 0;
 	
-	if (h == NULL)
-	{
-		printf("The list is empty!");
-		return (0);
-	}
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
