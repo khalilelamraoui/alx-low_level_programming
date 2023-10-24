828426001907c4866c272df8f35de273310b469b
khalilelamraoui <khalilelam16@gmail.com>
2023-10-24 06:11:47 -0700

    find_loop func

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * find_listint_loop - Finds the loop in a linked list using Floyd's algorithm.
+ * @head: A pointer to the head of the list.
+ * Return: The address of the node where the loop starts, or NULL if there is no loop.
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *slow = head;
+	listint_t *fast = head;
+
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (slow);
+		}
+	}
+	return (NULL);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -108,5 +108,12 @@ size_t print_listint_safe(const listint_t *head);
  * Return: The size of the list that was freed.
  */
 size_t free_listint_safe(listint_t **h);
+/**
+ * find_listint_loop - Finds the loop in a linked list using Floyd's algorithm.
+ * @head: A pointer to the head of the list.
+ * Return: The address of the node where the loop starts, or NULL if there is no loop.
+ */
+listint_t *find_listint_loop(listint_t *head);
+
 
 #endif
