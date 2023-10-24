0c2be8093521f2e027087d1fce295f558ae3f8aa
khalilelamraoui <khalilelam16@gmail.com>
2023-10-24 06:12:58 -0700

    find_loop func

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -2,7 +2,8 @@
 /**
  * find_listint_loop - Finds the loop in a linked list using Floyd's algorithm.
  * @head: A pointer to the head of the list.
- * Return: The address of the node where the loop starts, or NULL if there is no loop.
+ * Return: The address of the node where the loop starts,
+ * or NULL if there is no loop.
  */
 listint_t *find_listint_loop(listint_t *head)
 {
