180d82c2ee2da04db825ef280a302f0d65fea444
khalilelamraoui <khalilelam16@gmail.com>
2023-10-26 08:09:28 -0700

    get_bit func

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ * get_bit - Returns the value of a bit at a given index.
+ * @n: The unsigned long integer.
+ * @index: The index of the bit to retrieve (starting from 0).
+ * Return: The value of the bit at the given index (0 or 1) or -1 if an error occurred.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int mask;
+	int bit;
+
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+	mask = 1UL << index;
+	bit = (n & mask) ? 1 : 0;
+	return (bit);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -25,5 +25,13 @@ void print_binary(unsigned long int n);
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c);
+/**
+ * get_bit - Returns the value of a bit at a given index.
+ * @n: The unsigned long integer.
+ * @index: The index of the bit to retrieve (starting from 0).
+ * Return: The value of the bit at the given index (0 or 1) or -1 if an error occurred.
+ */
+int get_bit(unsigned long int n, unsigned int index);
+
 
 #endif
