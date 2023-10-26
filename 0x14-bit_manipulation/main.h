7ffc4ca6bdc032c89d52dff9d5c266924e4aacb0
khalilelamraoui <khalilelam16@gmail.com>
2023-10-26 08:12:00 -0700

    set_bit func

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -32,6 +32,13 @@ int _putchar(char c);
  * Return: The value of the bit at the given index (0 or 1) or -1 if an error occurred.
  */
 int get_bit(unsigned long int n, unsigned int index);
+/**
+ * set_bit - Sets the value of a bit to 1 at a given index.
+ * @n: A pointer to the unsigned long integer.
+ * @index: The index of the bit to set (starting from 0).
+ * Return: 1 if it worked, or -1 if an error occurred.
+ */
+int set_bit(unsigned long int *n, unsigned int index);
 
 
 #endif
