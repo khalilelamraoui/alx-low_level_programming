31d6135b9a94fed2766eed03f2f86faf81384789
khalilelamraoui <khalilelam16@gmail.com>
2023-10-26 08:20:33 -0700

    fixed betty for get_bit func

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,7 +3,8 @@
  * get_bit - Returns the value of a bit at a given index.
  * @n: The unsigned long integer.
  * @index: The index of the bit to retrieve (starting from 0).
- * Return: The value of the bit at the given index (0 or 1) or -1 if an error occurred.
+ * Return: The value of the bit at the given index (0 or 1)
+ * or -1 if an error occurred.
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
