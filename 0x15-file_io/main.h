56045c0c305fa7fcd17df3e9f05389a24b0dc0e2
khalilelamraoui <khalilelam16@gmail.com>
2023-10-30 05:11:22 -0700

    fixed main.h betty

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,8 +10,8 @@
  * read_textfile - Reads a text file and prints it to the POSIX standard output
  * @filename: The name of the file to read
  * @letters: The number of letters to read and print
- * Return: The actual number of letters it could read and print 
- * 0 if the file can't be opened or read, or if write fails or does not 
+ * Return: The actual number of letters it could read and print
+ * 0 if the file can't be opened or read, or if write fails or does not
  * write the expected amount of bytes, or if filename is NULL
  */
 ssize_t read_textfile(const char *filename, size_t letters);
