02abd27d787d5d345d65f6bd99855455ec4213ca
khalilelamraoui <khalilelam16@gmail.com>
2023-12-07 22:12:03 +0100

    fixed last advanced

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,34 +1,48 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
-#include <stddef.h>
 
-void generateKey(const char *username, char *key) {
-    int i = 0;
-    int j = 0;
-    int len = 0;
-    int tmp = 0;
-    int sum = 0;
-
-    while (username[i] != '\0') {
-        sum += username[i];
-        i++;
-    }
-    tmp = (sum ^ 0x3afc);
-    len = sprintf(key, "%d", tmp);
-    while (j < len) {
-        key[j] = key[j] + 0x3afc;
-        j++;
-    }
-}
-
-int main(int argc, char **argv) {
-    char key[100];
+int main(int argc, char *argv[]) {
+	char *charset = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	char key[7] = "      ";
+	size_t len = strlen(argv[1]);
+	unsigned int add = 0, b = 1;
+	size_t i;
 
     if (argc != 2) {
-        printf("Usage: %s username\n", argv[0]);
+        printf("Correct usage: ./keygen5 username\n");
         return 1;
     }
-    generateKey(argv[1], key);
+
+    key[0] = charset[(len ^ 59) & 63];
+
+    for (i = 0; i < len; i++)
+        add += argv[1][i];
+    key[1] = charset[(add ^ 79) & 63];
+
+    
+    for (i = 0; i < len; i++)
+        b *= argv[1][i];
+    key[2] = charset[(b ^ 85) & 63];
+
+    b = (unsigned char)argv[1][0];
+    for (i = 0; i < len; i++)
+        if ((unsigned char)b <= (unsigned char)argv[1][i])
+            b = (unsigned char)argv[1][i];
+    srand(b ^ 14);
+    key[3] = charset[rand() & 63];
+
+    b = 0;
+    for (i = 0; i < len; i++)
+        b += argv[1][i] * argv[1][i];
+    key[4] = charset[(b ^ 239) & 63];
+
+    b = 0;
+    for (i = 0; (unsigned char)i < (unsigned char)argv[1][0]; i++)
+        b = rand();
+    key[5] = charset[(b ^ 229) & 63];
+
     printf("%s\n", key);
+
     return 0;
 }
