386a1c5ee50d79130c1eb847cb443c9219eb8c1e
khalilelamraoui <khalilelam16@gmail.com>
2023-12-07 20:31:33 +0100

    fixed betty

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -6,12 +6,12 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-    size_t counter = 0;
+	size_t counter = 0;
 
-    while (h != NULL)
-    {
-        h = h->next;
-        counter++;
-    }
-    return (counter);
+	while (h != NULL)
+	{
+		h = h->next;
+		counter++;
+	}
+	return (counter);
 }
