385c6ebaf16e087c2dd091ef3a9c1ff4e66f19c0
khalilelamraoui <khalilelam16@gmail.com>
2023-12-11 13:07:17 +0100

    fixed some issues

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P ../ https://github.com/khalilelamraoui/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD=$PWD/libgiga.so
+export LD_PRELOAD=../libgiga.so
