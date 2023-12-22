30db1e7a2b3daab9535e9dc65ff04971d9b310c9
khalilelamraoui <khalilelam16@gmail.com>
2023-12-22 21:38:52 +0100

    added task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_print - prints a hash table
+* @ht: hash table to print
+*
+* Return: void
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+
+	hash_node_t *node;
+	char *sep = "";
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			printf("%s'%s': '%s'", sep, node->key, node->value);
+			sep = ", ";
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
