fb158fd6ae61a445e1f49db6397aa2b4bea83bdf
khalilelamraoui <khalilelam16@gmail.com>
2024-04-11 23:52:16 +0000

    fixed

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,36 +1,74 @@
 #include "search_algos.h"
+
+int advanced_binary_r(int *array, int start, int end, int value);
+void print_array(int *array, size_t start, size_t end);
+
 /**
- * advanced_binary - function that searches for
- * a value in a sorted array of integers.
- * @array : pointer to the first element of the array to search in.
- * @size : the number of elements in array.
- * @value : the value to search for.
- * Return: index of value or -1 if not found.
-*/
+ * advanced_binary - searches for a value in a sorted array of integers
+ * using the Binary search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in the array
+ * @value: value to search for
+ *
+ * Return: the index where the value is located, or -1 if value is not present
+ * in array
+ */
+
 int advanced_binary(int *array, size_t size, int value)
 {
-	size_t index = 0, s = size - 1, move, r;
+	if (!array)
+		return (-1);
 
-	if (array)
+	return (advanced_binary_r(array, 0, size - 1, value));
+}
+
+/**
+ * advanced_binary_r - searches for a value in a sorted array of integers
+ * using the Binary search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @start: starting index for binary search
+ * @end: ending index for binary search
+ * @value: value to search for
+ *
+ * Return: the index where the value is located, or -1 if value is not present
+ * in array
+ */
+
+int advanced_binary_r(int *array, int start, int end, int value)
+{
+	int mid;
+
+	if (start > end)
+		return (-1);
+
+	mid = start + (end - start) / 2;
+	print_array(array, start, end);
+
+	if (array[mid] == value)
 	{
-		while (index <= s)
-		{
-			printf("Searching in array: ");
-			for (r = index; r < s; r++)
-				printf("%d, ", array[r]);
-			printf("%d\n", array[r]);
-			move = (index + s) / 2;
-			if (array[move] == value)
-			{
-				if (move == 0 || array[move - 1] < value)
-					return (move);
-				s = move;
-			}
-			else if (array[move] < value)
-				index = move + 1;
-			else
-				s = move;
-		}
+		if (mid == 0 || array[mid - 1] != value)
+			return (mid);
+		else
+			return (advanced_binary_r(array, start, mid, value));
 	}
-	return (-1);
+	else if (array[mid] < value)
+		return (advanced_binary_r(array, mid + 1, end, value));
+	else
+		return (advanced_binary_r(array, start, mid - 1, value));
+}
+
+
+/**
+ * print_array - prints the elements of an array between two indices
+ * @array: pointer to the first element of the array
+ * @start: starting index
+ * @end: ending index
+ */
+void print_array(int *array, size_t start, size_t end)
+{
+	printf("Searching in array: ");
+	for (; start < end; start++)
+		printf("%d, ", array[start]);
+
+	printf("%d\n", array[start]);
 }
